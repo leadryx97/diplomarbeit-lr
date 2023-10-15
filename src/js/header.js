19b3d9d7f5e1fc80191d4b9ce5372c20a308cafa
Lea Ritter <leadryx@gmail.com>
2023-10-15 21:18:32 +0200

    header styles and javascript added

---
+++ src/js/header.js
@@ -0,0 +1,20 @@
+/*console.log("hellooo");
+
+const nav = document.querySelector(".header__nav-menu");
+const navItems = document.querySelector(".header__nav-link");
+const hamburger = document.querySelector(".header__nav-hamburger");
+const hamburgerIcon = document.querySelector(".header__nav-hamburger-img");
+const closeIcon = document.querySelector(".header__nav-hamburger-img--close");
+
+function toggleMenu() {
+  if (nav.classList.contains("header__nav-menu--show")) {
+    nav.classList.remove("header__nav-menu--show");
+    closeIcon.style.display = "none";
+    hamburgerIcon.style.display = "block";
+  } else {
+    nav.classList.add(".header__nav-menu--show");
+    closeIcon.style.display = "block";
+    hamburgerIcon.style.display = "none";
+  }
+}
+*/
