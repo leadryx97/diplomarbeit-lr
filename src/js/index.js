36bcf9f126a1960a10e44fa18c7f9c78fa447dab
Lea Ritter <leadryx@gmail.com>
2023-10-17 14:49:00 +0200

    header/navigation styles customized

--- src/js/index.js
+++ src/js/index.js
@@ -6,16 +6,19 @@ const navItems = document.querySelectorAll(".header__nav-link");
 const hamburger = document.querySelector(".header__nav-hamburger");
 const hamburgerIcon = document.querySelector(".header__nav-hamburger-img");
 const closeIcon = document.querySelector(".header__nav-hamburger-img--close");
+const body = document.querySelector("body");
 
 function toggleMenu() {
   if (nav.classList.contains("header__nav-menu--show")) {
     nav.classList.remove("header__nav-menu--show");
     closeIcon.style.display = "none";
     hamburgerIcon.style.display = "block";
+    body.style.overflowY = "visible";
   } else {
     nav.classList.add("header__nav-menu--show");
     closeIcon.style.display = "block";
     hamburgerIcon.style.display = "none";
+    body.style.overflowY = "hidden";
   }
 }
 hamburger.addEventListener("click", toggleMenu);
