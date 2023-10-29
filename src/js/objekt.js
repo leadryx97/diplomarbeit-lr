bac2200fbab486fd78ff522a4a8ed1456a2b841a
Lea Ritter <leadryx@gmail.com>
2023-10-29 13:31:11 +0100

    added object page styles

--- src/js/objekt.js
+++ src/js/objekt.js
@@ -9,3 +9,79 @@ import {
   toggleMenu,
   setActiveItem,
 } from "./header.js";
+
+// import location icon
+import markerIcon from "../assets/images/svg/map-location-marker.svg";
+
+// google maps
+((g) => {
+  var h,
+    a,
+    k,
+    p = "The Google Maps JavaScript API",
+    c = "google",
+    l = "importLibrary",
+    q = "__ib__",
+    m = document,
+    b = window;
+  b = b[c] || (b[c] = {});
+  var d = b.maps || (b.maps = {}),
+    r = new Set(),
+    e = new URLSearchParams(),
+    u = () =>
+      h ||
+      (h = new Promise(async (f, n) => {
+        await (a = m.createElement("script"));
+        e.set("libraries", [...r] + "");
+        for (k in g)
+          e.set(
+            k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+            g[k]
+          );
+        e.set("callback", c + ".maps." + q);
+        a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+        d[q] = f;
+        a.onerror = () => (h = n(Error(p + " could not load.")));
+        a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+        m.head.append(a);
+      }));
+  d[l]
+    ? console.warn(p + " only loads once. Ignoring:", g)
+    : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+})({
+  key: "AIzaSyAWGZmIJEj81w1nCFltoYGn-V_PXQHVae0",
+});
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // location
+  const position = { lat: 47.178561307288355, lng: 9.451303899836864 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at home & house
+  map = new Map(document.querySelector(".property-information__map"), {
+    zoom: 15.7,
+    center: position,
+    disableDefaultUI: true,
+    mapId: "560e1e5e3f75ad8c",
+  });
+
+  // The marker, positioned at home & house
+  const iconSize = new google.maps.Size(50, 50);
+
+  new google.maps.Marker({
+    position: position,
+    map,
+    title: "Hello World!",
+    icon: {
+      url: markerIcon,
+      scaledSize: iconSize,
+    },
+  });
+}
+
+initMap();
